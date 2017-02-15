9edb7f5504dcdf1cd2273b9c73c3b3384c006f82
Jonathan Muller <jonathan@cubyn.com>
2017-02-15 16:56:37 +0100

    Ignore rule extraneousDeps on specs i Pleas enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   rules: {
-    'no-logger-log': require('./lib/rules/no-logger-log')
+    'no-logger-log': require('./lib/rules/no-logger-log'),
+    'import/no-extraneous-dependencies': [
+        "error",
+        { "devDependencies": [ "**/*.spec.js" ] }
+    ]
   }
 };
