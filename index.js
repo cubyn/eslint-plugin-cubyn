f7440fed955ae057b640d2c0a903cdda4be2bc43
FPierre <pflauder@gmail.com>
2018-11-25 19:08:04 +0100

    Improve naming

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const awaitInvoke = require('./lib/rules/await-invoke');
 const awaitPublish = require('./lib/rules/await-publish');
 const limitBeforeFirst = require('./lib/rules/limit-before-first');
-const meta = require('./lib/rules/meta');
 const metaDescription = require('./lib/rules/meta-description');
 const metaPermissions = require('./lib/rules/meta-permissions');
+const metaPresence = require('./lib/rules/meta-presence');
 
 module.exports = {
   configs: {
     recommended: {
+      extends: [
+        'airbnb-base',
+      ],
       plugins: ['cubyn'],
+      env: {
+        es6: true,
+        jest: true,
+        node: true,
+      },
       rules: {
         'cubyn/await-invoke': 'off',
         'cubyn/await-publish': 'off',
         'cubyn/limit-before-first': 'off',
-        'cubyn/meta': 'off',
         'cubyn/meta-description': 'off',
         'cubyn/meta-permissions': 'off',
+        'cubyn/meta-presence': 'off',
+
+        'max-depth': ['error', 3],
+        'max-len': ['error', 100, 2, { ignoreStrings: false }],
+        'no-param-reassign': 'off',
+        'no-plusplus': 'off',
+        'no-use-before-define': 'off',
+        complexity: ['error', 5],
+        'object-curly-newline': ['error', {
+          ObjectExpression: { consistent: true, multiline: true },
+          ObjectPattern: { consistent: true, multiline: true },
+        }],
       },
       overrides: [
         {
@@ -37,9 +56,9 @@ module.exports = {
             'src/listeners/**/index.js',
           ],
           rules: {
-            'cubyn/meta': 'error',
             'cubyn/meta-description': 'error',
             'cubyn/meta-permissions': 'error',
+            'cubyn/meta-presence': 'error',
           },
         },
       ],
@@ -49,8 +68,8 @@ module.exports = {
     'await-invoke': awaitInvoke,
     'await-publish': awaitPublish,
     'limit-before-first': limitBeforeFirst,
-    meta,
     'meta-description': metaDescription,
     'meta-permissions': metaPermissions,
+    'meta-presence': metaPresence,
   },
 };
