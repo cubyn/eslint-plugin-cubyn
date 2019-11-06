d51816357da43190d0e84ed1cb80247d010d1e33
FPierre <pflauder@gmail.com>
2019-11-06 14:35:03 +0100

    Improve rules visibility

--- index.js
+++ index.js
@@ -4,6 +4,72 @@ const metaPermissions = require('./lib/rules/meta-permissions');
 const noInvokeTopic = require('./lib/rules/no-invoke-topic');
 const transaction = require('./lib/rules/transaction');
 
+const RULES = {
+  CUBYN: {
+    // Activations/updates
+    'cubyn/logger-context': 'error',
+    'cubyn/logger-error': 'error',
+    'cubyn/no-invoke-topic': 'error',
+    'cubyn/transaction': 'error',
+    // Deactivations
+    'cubyn/meta-permissions': 'off',
+  },
+  UNICORN: {
+    // Activations/updates
+    'unicorn/import-index': 'error',
+    'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/prefer-includes': 'error',
+    'unicorn/regex-shorthand': 'error',
+    'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+  },
+  AIRBNB: {
+    // Activations/updates
+    'no-eq-null': 'error',
+    'no-implicit-globals': 'error',
+    'no-shadow': 'error',
+    'no-undefined': 'error',
+    'no-useless-call': 'error',
+    'no-useless-catch': 'error',
+    'prefer-object-spread': 'error',
+    complexity: ['error', 10],
+    'max-depth': ['error', 4],
+    'max-statements-per-line': ['error', { max: 1 }],
+    'max-nested-callbacks': ['error', { max: 4 }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+    'id-length': ['error', {
+      min: 3,
+      properties: 'never',
+      exceptions: ['fs', 'os', 'vm', 'i', 'id'],
+    }],
+    'line-comment-position': ['error', {
+      position: 'above',
+      applyDefaultPatterns: true,
+    }],
+    'no-magic-numbers': ['error', {
+      ignoreArrayIndexes: true,
+      ignore: [-1, 0, 1, 2, 10],
+      enforceConst: true,
+    }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: { consistent: true, multiline: true },
+      ObjectPattern: { consistent: true, multiline: true },
+    }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var', 'expression'] },
+    ],
+    // Deactivations
+    'no-plusplus': 'off',
+    'no-use-before-define': 'off',
+  },
+};
+
 module.exports = {
   configs: {
     recommended: {
@@ -19,96 +85,9 @@ module.exports = {
         node: true,
       },
       rules: {
-        //
-        // Cubyn rules
-        //
-
-        // Activations/updates
-
-        'cubyn/logger-context': 'error',
-        'cubyn/logger-error': 'error',
-        'cubyn/no-invoke-topic': 'error',
-        'cubyn/transaction': 'error',
-
-        // Deactivations
-
-        'cubyn/meta-permissions': 'off',
-
-        //
-        // Unicorn rules
-        //
-
-        // Activations/updates
-
-        'unicorn/filename-case': ['error', { case: 'kebabCase' }],
-        'unicorn/import-index': 'error',
-        'unicorn/no-array-instanceof': 'error',
-        'unicorn/no-for-loop': 'error',
-        'unicorn/no-new-buffer': 'error',
-        'unicorn/prefer-includes': 'error',
-        'unicorn/regex-shorthand': 'error',
-
-        //
-        // Airbnb rules
-        //
-
-        // Activations/updates
-
-        'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-        'id-length': ['error', {
-          min: 3,
-          properties: 'never',
-          exceptions: ['fs', 'os', 'vm', 'i', 'id'],
-        }],
-        'line-comment-position': ['error', {
-          position: 'above',
-          applyDefaultPatterns: true,
-        }],
-        'max-depth': ['error', 4],
-        'max-nested-callbacks': ['error', { max: 4 }],
-        'max-statements-per-line': ['error', { max: 1 }],
-        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-        'no-eq-null': 'error',
-        'no-implicit-globals': 'error',
-        'no-magic-numbers': ['error', {
-          ignoreArrayIndexes: true,
-          ignore: [-1, 0, 1, 2, 10],
-          enforceConst: true,
-        }],
-        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-        'no-shadow': 'error',
-        'no-undefined': 'error',
-        'no-useless-call': 'error',
-        'no-useless-catch': 'error',
-        'object-curly-newline': ['error', {
-          ObjectExpression: { consistent: true, multiline: true },
-          ObjectPattern: { consistent: true, multiline: true },
-        }],
-        'padding-line-between-statements': [
-          'error',
-          {
-            blankLine: 'always',
-            prev: '*',
-            next: 'return',
-          },
-          {
-            blankLine: 'always',
-            prev: ['const', 'let', 'var'],
-            next: '*',
-          },
-          {
-            blankLine: 'any',
-            prev: ['const', 'let', 'var'],
-            next: ['const', 'let', 'var', 'expression'],
-          },
-        ],
-        'prefer-object-spread': 'error',
-        complexity: ['error', 10],
-
-        // Deactivations
-
-        'no-plusplus': 'off',
-        'no-use-before-define': 'off',
+        ...RULES.CUBYN,
+        ...RULES.UNICORN,
+        ...RULES.AIRBNB,
       },
       overrides: [
         {
